c80b73b6c822a0285aa7423bef50771d4c0b1301
HEIRARCH <adityasirsat1234@gmail.com>
2024-10-21 01:43:18 +0530

    fixed hai

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-// module.exports = {
-//   async redirects() {
-//     return [
-//       {
-//         source: '/',
-//         destination: '/about',
-//         permanent: true,
-//       },
-//     ]
-//   },
-// }
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
+}
