48cddfcf5624ca982f19e80a029b53c94a4c0754
HEIRARCH <adityasirsat1234@gmail.com>
2024-10-22 00:52:50 +0530

    v5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 import HeroSection from './components/HeroSection'; // Assuming you have uploaded this
 import Footer from './components/Footer'; // Assuming you have uploaded this
 import Header from './components/Header';
-import LatestQuestions from './components/LatestQuestions';
-import TopContributers from './components/TopContributers';
 
 export default function Home() {
   return (
