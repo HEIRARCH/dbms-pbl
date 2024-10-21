bd875d26f08248f280875c8b1a31455c504b95f4
HEIRARCH <adityasirsat1234@gmail.com>
2024-10-22 01:22:09 +0530

    v10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 
+import Footer from './components/Footer'; // Assuming you have uploaded this
+import Header from './components/Header';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        HELLO NA
-      </div>
+    <main>
+      {/* Hero Section - This is usually the first section of the homepage */}
+      <Header />
+      
+      <Footer />
     </main>
   );
 }
