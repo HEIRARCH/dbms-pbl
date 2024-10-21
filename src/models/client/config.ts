a1ad0bea43defa729d81426328c3e9ad6779fd69
HEIRARCH <adityasirsat1234@gmail.com>
2024-10-22 00:27:50 +0530

    v3

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -2,7 +2,7 @@ import env from "@/app/env";
 
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
 
-const client = new Client()
+let client = new Client()
     .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
     .setProject(env.appwrite.projectId); // Your project ID
 
